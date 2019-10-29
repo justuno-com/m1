d03dba94179891e0b47b856b501e73e575128053
Dmitry Fedyuk <dfediuk@gmail.com>
2019-10-30 02:54:25 +0600

    1.1.8: https://github.com/justuno-com/m1/issues/7

--- app/code/community/Justuno/Jumagext/Catalog/Variants.php
+++ app/code/community/Justuno/Jumagext/Catalog/Variants.php
@@ -16,8 +16,8 @@ final class Justuno_Jumagext_Catalog_Variants {
 		else {
 			$ct = $p->getTypeInstance(); /** @var Mage_Catalog_Model_Product_Type_Configurable $ct */
 			$opts = array_column($ct->getConfigurableAttributesAsArray($p), 'attribute_code', 'id');
-			$r = array_values(array_map(function(P $p) use($opts) {return
-				self::variant($p, $opts)
+			$r = array_values(array_map(function(P $c) use($opts, $p) {return
+				self::variant($c, $p, $opts)
 			;}, $ct->getUsedProducts(null, $p)));
 		}
 		return $r;
@@ -26,10 +26,11 @@ final class Justuno_Jumagext_Catalog_Variants {
 	/**
 	 * 2019-10-30
 	 * @param P $p
+	 * @param P|null $parent [optional]
 	 * @param array(int => string) $opts [optional]
 	 * @return array(string => mixed)
 	 */
-	private static function variant(P $p, $opts = []) {
+	private static function variant(P $p, P $parent = null, $opts = []) {
 		// 2019-08-28 Otherwise $p does not contain the product's price
 		$p = $p->load($p->getId()); /** @var P $p */
 		$si = new SI; /** @var SI $si */
@@ -37,10 +38,16 @@ final class Justuno_Jumagext_Catalog_Variants {
 		$r = [
 			'ID' => $p->getId()
 			,'InventoryQuantity' => (int)$si->getQty()
-			// 2019-10-30
-			// «MSRP, Price, SalePrice, Variants.MSRP, and Variants.SalePrice all need to be Floats,
-			// or if that is not possible then Ints»: https://github.com/justuno-com/m1/issues/10
-			,'MSRP' => (float)$p['msrp']
+			/**
+			 * 2019-10-30
+			 * 1) «MSRP, Price, SalePrice, Variants.MSRP, and Variants.SalePrice all need to be Floats,
+			 * or if that is not possible then Ints»: https://github.com/justuno-com/m1/issues/10
+			 * 2) «MSRP was null for some variants but the MSRP wasn't null for the parent»:
+			 * https://github.com/justuno-com/m1/issues/7
+			 * 3) «If their isn't an MSRP for some reason just use the salesprice»:
+			 * https://github.com/justuno-com/m1/issues/6
+			 */
+			,'MSRP' => (float)($p['msrp'] ?: ($parent ? $parent['msrp'] : $p->getPrice()))
 			// 2019-10-30
 			// «MSRP, Price, SalePrice, Variants.MSRP, and Variants.SalePrice all need to be Floats,
 			// or if that is not possible then Ints»: https://github.com/justuno-com/m1/issues/10
