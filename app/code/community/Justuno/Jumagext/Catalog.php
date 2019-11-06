ce700bc2347a99c11e11f7949fa1a352478bd5f6
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-06 13:37:22 +0600

    no message

--- app/code/community/Justuno/Jumagext/Catalog.php
+++ app/code/community/Justuno/Jumagext/Catalog.php
@@ -111,8 +111,7 @@ final class Justuno_Jumagext_Catalog {
 				 * We need these sent back starting at OptionType1, OptionType2»:
 				 * https://github.com/justuno-com/m1/issues/14
 				 */
-				foreach (array_values($opts) as $id => $code) {
-					$id++;
+				foreach (array_values($opts) as $id => $code) {$id++; /** @var int $id */ /** @var string $code */
 					$r["OptionType$id"] = $code;
 				}
 			}
