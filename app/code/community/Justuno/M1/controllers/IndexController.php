367d9723b73f0a8209a15ae4dbca3feb1efe65dc
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-26 02:37:46 +0600

    no message

--- app/code/community/Justuno/M1/controllers/IndexController.php
+++ app/code/community/Justuno/M1/controllers/IndexController.php
@@ -2,7 +2,7 @@
 final class Justuno_M1_IndexController extends Mage_Core_Controller_Front_Action {
 	/**
 	 * 2019-11-26
-	 * @used-by app/design/frontend/base/default/template/justuno/m1/embed.phtml
+	 * @used-by app/design/frontend/base/default/template/justuno/m1.phtml
 	 */
     function getcartAction(){
     	$res = $this->getResponse(); /** @var Mage_Core_Controller_Response_Http $res */
