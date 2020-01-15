c252db7b8e1845257615a9c1dcea9739b2c89009
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-15 12:18:37 +0600

    no message

--- app/code/community/Justuno/M1/controllers/ResponseController.php
+++ app/code/community/Justuno/M1/controllers/ResponseController.php
@@ -4,9 +4,13 @@ final class Justuno_M1_ResponseController extends Mage_Core_Controller_Front_Act
 	 * 2019-10-27
 	 * /jumagext/response/catalog?pageSize=2&currentPage=1&sortProducts=entity_id&updatedSince=2014-01-01
 	 * https://www.upwork.com/messages/rooms/room_e6b2d182b68bdb5e9bf343521534b1b6/story_2e22707221dd053eab677398848c8ea3
+	 * 2020-01-15 https://support.justuno.com/ai-upsell-cross-sell-product-recommendations-plus-feeds
 	 */
 	function catalogAction() {Justuno_M1_Catalog::p();}
 
-	/** 2019-10-27 */
+	/**
+	 * 2019-10-27
+	 * 2020-01-15 https://support.justuno.com/ai-upsell-cross-sell-product-recommendations-plus-feeds
+	 */
 	function ordersAction() {Justuno_M1_Orders::p();}
 }
